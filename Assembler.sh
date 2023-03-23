58cca4317ead0dcbba9021ce6347fe127836e92b
ENRORMA <105936026+ENRORMA@users.noreply.github.com>
2023-03-23 11:48:22 +0100

    changed python to python3

--- Assembler.sh
+++ Assembler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 if [ "$1" == "" ]; then
-  python "$SCRIPT_PATH/Assemblers/TextAssembler.py"
+  python3 "$SCRIPT_PATH/Assemblers/TextAssembler.py"
 else
-  python "$SCRIPT_PATH/Assemblers/FileAssembler.py" $1
+  python3 "$SCRIPT_PATH/Assemblers/FileAssembler.py" $1
 fi
